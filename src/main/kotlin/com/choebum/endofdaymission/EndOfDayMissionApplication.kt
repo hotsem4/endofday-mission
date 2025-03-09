4fec0e2049afca2eae83e55911aa937314b82b75
Bumsu Choe <hotsem100@gmail.com>
2025-03-09 15:42:37 +0900

    [미션1] 깃허브 리포지토리에 프로젝트 올리기

---
+++ src/main/kotlin/com/choebum/endofdaymission/EndOfDayMissionApplication.kt
@@ -0,0 +1,11 @@
+package com.choebum.endofdaymission
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EndOfDayMissionApplication
+
+fun main(args: Array<String>) {
+    runApplication<EndOfDayMissionApplication>(*args)
+}
---
+++ src/test/kotlin/com/choebum/endofdaymission/EndOfDayMissionApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.choebum.endofdaymission
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EndOfDayMissionApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
